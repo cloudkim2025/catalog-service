4499333dce92e79d2050c91c5b036171535e3051
cloudkim2025 <cloudviola@icloud.com>
2025-03-26 11:21:01 +0900

    Update project files

--- Polar-Service/web-front-service/src/main/java/com/example/webfrontservice/WebFrontServiceApplication.java
+++ Polar-Service/web-front-service/src/main/java/com/example/webfrontservice/WebFrontServiceApplication.java
@@ -1,8 +1,11 @@
 package com.example.webfrontservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class WebFrontServiceApplication {
 
--- Polar-Service/web-front-service/src/main/java/com/example/webfrontservice/controller/UserApiController.java
+++ Polar-Service/web-front-service/src/main/java/com/example/webfrontservice/controller/UserApiController.java
@@ -1,9 +1,41 @@
 package com.example.webfrontservice.controller;
 
+import com.example.webfrontservice.dto.*;
+import com.example.webfrontservice.service.UserService;
+import com.example.webfrontservice.utill.CookieUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public JoinResponseDTO join(@RequestBody JoinRequestDTO joinRequestDTO) {
+        return userService.join(joinRequestDTO)
+                .toJoinResponseDTO();
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO loin(
+            HttpServletResponse response,
+            @RequestMapping LoginRequestDTO loginRequestDTO
+    ){
+        LoginClientResponseDTO logined = userService.login(loginRequestDTO);
+
+        if (logined != null && logined.isLoggedId()) {
+            CookieUtil.addCookie(response "refreshToken",logined.getRefreshToken(), 7*24*60*60);
+        }
+
+        assert logined != null;
+        return logined.toLoginResponseDTO();
+
+    }
+
 }
